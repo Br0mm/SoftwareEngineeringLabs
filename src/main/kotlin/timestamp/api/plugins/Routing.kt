cb8548dde8bb8c277a0b5c5ad19139de2ee6ce72
Br0mm <andrew-ost@mail.ru>
2022-12-04 00:37:47 +0300

    #4 added timestamp as parameter

--- src/main/kotlin/timestamp/api/plugins/Routing.kt
+++ src/main/kotlin/timestamp/api/plugins/Routing.kt
@@ -2,11 +2,11 @@ package timestamp.api.plugins
 
 import io.ktor.application.*
 import io.ktor.routing.*
-import timestamp.api.routes.currentTimestamp
+import timestamp.api.routes.timestamp
 
 fun Application.configureRouting() {
 
     routing {
-        currentTimestamp()
+        timestamp()
     }
 }
