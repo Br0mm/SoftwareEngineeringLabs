c59d78d14ab76395661f9b78e161df42492d9072
Br0mm <andrew-ost@mail.ru>
2022-12-17 17:57:49 +0300

    #19 fixed unstable test

--- src/test/kotlin/timestamp/api/Tests.kt
+++ src/test/kotlin/timestamp/api/Tests.kt
@@ -15,7 +15,7 @@ class Tests {
         val response = client.get("/timestamp")
         assertEquals(HttpStatusCode.OK, response.status)
 
-        val regex = Regex("""^\d{4}-\d{2}-\d{2}T(\d{2}:){2}\d{2}.\d{3}$""")
+        val regex = Regex("""^\d{4}-\d{2}-\d{2}T(\d{2}:){2}\d{2}(.\d{0,3})?$""")
 
         assertEquals(
             true,
